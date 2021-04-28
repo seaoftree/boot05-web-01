e55ac4eda465bc5fbc72e03b32ff3890b9868a4d
lg <768192412@qq.com>
2021-04-28 20:41:13 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/boot/test/Test.java
+++ src/main/java/com/atguigu/boot/test/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }

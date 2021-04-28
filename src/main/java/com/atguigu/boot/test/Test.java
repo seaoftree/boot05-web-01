4555ee203cd95548177995950af929816acf2bd3
lg <768192412@qq.com>
2021-04-28 20:27:16 +0800

    third commit

--- src/main/java/com/atguigu/boot/test/Test.java
+++ src/main/java/com/atguigu/boot/test/Test.java
@@ -6,5 +6,6 @@ package com.atguigu.boot.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }

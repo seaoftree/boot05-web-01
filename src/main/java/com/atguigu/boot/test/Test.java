9cfe1e33887b77084fa325d33222789bbcf8ab01
lg <768192412@qq.com>
2021-04-28 20:26:23 +0800

    second commit

---
+++ src/main/java/com/atguigu/boot/test/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.boot.test;
+
+/**
+ * Ceaate by 76819 on 2021/4/28
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}

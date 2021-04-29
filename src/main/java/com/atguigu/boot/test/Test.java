c3ea5196c1b162fa9e78fb1ed9324f83359aa73b
自由的淦淦 <49030870+seaoftree@users.noreply.github.com>
2021-04-29 13:05:59 +0800

    Update Test.java

--- src/main/java/com/atguigu/boot/test/Test.java
+++ src/main/java/com/atguigu/boot/test/Test.java
@@ -13,5 +13,6 @@ public class Test {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee test");
+        System.out.println("github test");
     }
 }

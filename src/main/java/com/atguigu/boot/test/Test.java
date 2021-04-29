3044b815c23534f93d46a90844f68b90b0493bc4
自由的淦淦 <768192412@qq.com>
2021-04-29 12:56:52 +0800

    update src/main/java/com/atguigu/boot/test/Test.java.

--- src/main/java/com/atguigu/boot/test/Test.java
+++ src/main/java/com/atguigu/boot/test/Test.java
@@ -12,5 +12,6 @@ public class Test {
         System.out.println("master test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
